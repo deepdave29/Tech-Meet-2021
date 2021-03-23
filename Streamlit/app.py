64016a8194159395ec9894a8cf57fa03e528c8b0
Reuben27 <reuben.sd@iitgn.ac.in>
2021-03-23 17:16:38 +0530

    rotation function added

---
+++ Streamlit/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import cv2 as cv2 
+from PIL import Image
+
+def rotation(img, degree):
+    image = Image.open(img)
+    img_array = np.array(image)    
+    rows,cols, temp = img_array.shape
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),degree,1)
+    dst = cv2.warpAffine(img_array,M,(cols,rows))
+    st.image(dst)
+
+st.title('Inter IIT Tech Meet 2021')
+
+uploaded_files = st.file_uploader("Upload images", accept_multiple_files = True)
+percent_testing = st.slider('Select % of images for training', 0, 100, step=25)
+
+rotating_degree = st.slider('Select rotation degree', -45, 45, step=1, value=0)
+rotater = st.button('Rotate')
+if(rotater):
+    for uploaded_file in uploaded_files:
+        rotation(uploaded_file, rotating_degree)
+    
