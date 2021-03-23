855a678f738c4773341f3dc8b7221061d96ccd74
deepdave29 <68187780+deepdave29@users.noreply.github.com>
2021-03-23 20:30:58 +0530

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -13,12 +13,21 @@ def rotation(img, degree):
     dst = cv2.warpAffine(img_array,M,(cols,rows))
     st.image(dst)
 
-def translation(img):
+def translation(img, tx, ty):
     #translation function
     #to be completed
     image = Image.open(img)
     img_array = np.array(image)    
     rows,cols, temp = img_array.shape
+    
+    ## code 
+    
+    M = np.float32([1,0,tx] , [0,1,ty])
+    shifted = cv2.warpAffine(img_array,M,(cols,rows))
+    st.image(shifted)
+    
+    
+    ##code
 
 def blurring(img):
     #blurring function
@@ -26,6 +35,12 @@ def blurring(img):
     image = Image.open(img)
     img_array = np.array(image)    
     rows,cols, temp = img_array.shape
+    
+    ##code - Gausian Blurring
+    gausBlur = cv2.GaussianBlur(img_array, (5,5),0)
+    
+    
+    ##code
 
 def brightness(img):
     #function to increase/decrease the brightness of the image
@@ -33,14 +48,23 @@ def brightness(img):
     image = Image.open(img)
     img_array = np.array(image)    
     rows,cols, temp = img_array.shape
+    
+    ##code 
+    
+    ##code
 
-
-def zoom(img):
+def zoom(img, width, height):
     #function to zoom in/out the image with limit.
     #to be completed
     image = Image.open(img)
     img_array = np.array(image)    
     rows,cols, temp = img_array.shape
+    
+    ##code interpolation 
+
+    stretch_near = cv2.resize(image_array, (width, height),interpolation = cv2.INTER_NEAREST)
+    ##code
+    
 
 
 st.title('Inter IIT Tech Meet 2021')
